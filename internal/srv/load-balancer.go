04dfef790f514c0cdd09a1e6dc2ebc1003454ecb
Tyler Auerbeck <tylerauerbeck@users.noreply.github.com>
2023-03-30 09:32:51 -0400

    Some initial tests
    Signed-off-by: Tyler Auerbeck <tylerauerbeck@users.noreply.github.com>

--- internal/srv/load-balancer.go
+++ internal/srv/load-balancer.go
@@ -1,11 +1,8 @@
 package srv
 
 import (
-	"github.com/spf13/viper"
 	"go.infratographer.com/x/pubsubx"
 	"go.infratographer.com/x/urnx"
-
-	events "go.infratographer.com/loadbalanceroperator/pkg/events/v1alpha1"
 )
 
 func (s *Server) processLoadBalancer(msg pubsubx.Message) error {
@@ -31,7 +28,7 @@ func (s *Server) processLoadBalancer(msg pubsubx.Message) error {
 }
 
 func (s *Server) processLoadBalancerCreate(msg pubsubx.Message) error {
-	lbdata := events.LoadBalancerData{}
+	// lbdata := events.LoadBalancerData{}
 
 	lbURN, err := urnx.Parse(msg.SubjectURN)
 	if err != nil {
@@ -39,32 +36,33 @@ func (s *Server) processLoadBalancerCreate(msg pubsubx.Message) error {
 		return err
 	}
 
-	if err := s.parseLBData(&msg.AdditionalData, &lbdata); err != nil {
-		s.Logger.Errorw("handler unable to parse loadbalancer data", "error", err)
-		return err
-	}
+	// if err := s.parseLBData(&msg.AdditionalData, &lbdata); err != nil {
+	// 	s.Logger.Errorw("handler unable to parse loadbalancer data", "error", err)
+	// 	return err
+	// }
 
 	if _, err := s.CreateNamespace(lbURN.ResourceID.String()); err != nil {
 		s.Logger.Errorw("handler unable to create required namespace", "error", err)
 		return err
 	}
 
-	overrides := []valueSet{}
-	for _, cpuFlag := range viper.GetStringSlice("helm-cpu-flag") {
-		overrides = append(overrides, valueSet{
-			helmKey: cpuFlag,
-			value:   lbdata.Resources.CPU,
-		})
-	}
+	// overrides := []valueSet{}
+	// for _, cpuFlag := range viper.GetStringSlice("helm-cpu-flag") {
+	// 	overrides = append(overrides, valueSet{
+	// 		helmKey: cpuFlag,
+	// 		value:   lbdata.Resources.CPU,
+	// 	})
+	// }
 
-	for _, memFlag := range viper.GetStringSlice("helm-memory-flag") {
-		overrides = append(overrides, valueSet{
-			helmKey: memFlag,
-			value:   lbdata.Resources.Memory,
-		})
-	}
+	// for _, memFlag := range viper.GetStringSlice("helm-memory-flag") {
+	// 	overrides = append(overrides, valueSet{
+	// 		helmKey: memFlag,
+	// 		value:   lbdata.Resources.Memory,
+	// 	})
+	// }
 
-	if err := s.newDeployment(lbURN.ResourceID.String(), overrides); err != nil {
+	// if err := s.newDeployment(lbURN.ResourceID.String(), overrides); err != nil {
+	if err := s.newDeployment(lbURN.ResourceID.String(), nil); err != nil {
 		s.Logger.Errorw("handler unable to create loadbalancer", "error", err)
 		return err
 	}
@@ -88,5 +86,11 @@ func (s *Server) processLoadBalancerDelete(msg pubsubx.Message) error {
 }
 
 func (s *Server) processLoadBalancerUpdate(msg pubsubx.Message) error {
+	_, err := urnx.Parse(msg.SubjectURN)
+	if err != nil {
+		s.Logger.Errorw("unable to parse load-balancer URN", "error", err)
+		return err
+	}
+
 	return nil
 }
--- internal/srv/load-balancer_test.go
+++ internal/srv/load-balancer_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 	"github.com/nats-io/nats.go"
 	"github.com/stretchr/testify/assert"
 	"go.infratographer.com/loadbalanceroperator/internal/utils"
@@ -52,28 +53,56 @@ func (suite srvTestSuite) TestProcessLoadBalancer() {
 			name:        "create message",
 			expectError: false,
 			msg: pubsubx.Message{
-				EventType: create,
+				EventType:  create,
+				SubjectURN: "urn:infratographer:load-balancer:07442309-182E-4498-BC41-EBD679A9A792",
+			},
+		},
+		{
+			name:        "create failure",
+			expectError: true,
+			msg: pubsubx.Message{
+				EventType:  create,
+				SubjectURN: "thisisnonsense",
 			},
 		},
 		{
 			name:        "update message",
 			expectError: false,
 			msg: pubsubx.Message{
-				EventType: update,
+				EventType:  update,
+				SubjectURN: "urn:infratographer:load-balancer:07442309-182E-4498-BC41-EBD679A9A792",
+			},
+		},
+		{
+			name:        "update failure",
+			expectError: true,
+			msg: pubsubx.Message{
+				EventType:  update,
+				SubjectURN: "thisisnonsense",
 			},
 		},
 		{
 			name:        "delete message",
 			expectError: false,
 			msg: pubsubx.Message{
-				EventType: delete,
+				EventType:  delete,
+				SubjectURN: "urn:infratographer:load-balancer:07442309-182E-4498-BC41-EBD679A9A792",
+			},
+		},
+		{
+			name:        "delete failure",
+			expectError: true,
+			msg: pubsubx.Message{
+				EventType:  delete,
+				SubjectURN: "thisisnonsense",
 			},
 		},
 		{
 			name:        "unknown message",
 			expectError: true,
 			msg: pubsubx.Message{
-				EventType: "unknown",
+				EventType:  "unknown",
+				SubjectURN: "urn:infratographer:load-balancer:" + uuid.NewString(),
 			},
 		},
 	}
@@ -82,14 +111,74 @@ func (suite srvTestSuite) TestProcessLoadBalancer() {
 		suite.T().Run(tc.name, func(t *testing.T) {
 			s := srv
 			err := s.processLoadBalancer(tc.msg)
-			assert.Nil(suite.T(), err)
+
+			if tc.expectError {
+				assert.Error(suite.T(), err)
+			} else {
+				assert.Nil(suite.T(), err)
+			}
+
 		})
 	}
 }
 
-func (suite srvTestSuite) TestLoadBalancerUpdate() {
-	s := Server{}
-	msg := pubsubx.Message{}
-	err := s.processLoadBalancerUpdate(msg)
-	assert.Nil(suite.T(), err)
+func (suite srvTestSuite) TestProcessLoadBalancerCreate() {
+	type testCase struct {
+		name        string
+		msg         pubsubx.Message
+		expectError bool
+	}
+
+	js := utils.GetJetstreamConnection(suite.NATSServer)
+
+	_, _ = js.AddStream(&nats.StreamConfig{
+		Name:     "TestProcessLB",
+		Subjects: []string{"clb.foo", "clb.bar"},
+		MaxBytes: 1024,
+	})
+
+	dir, cp, ch, pwd := utils.CreateWorkspace("test-process-lb")
+	defer os.RemoveAll(dir)
+
+	srv := Server{
+		Gin:             gin.Default(),
+		Context:         context.TODO(),
+		StreamName:      "TestCreateLB",
+		Logger:          zap.NewNop().Sugar(),
+		KubeClient:      suite.Kubeenv.Config,
+		JetstreamClient: js,
+		Debug:           false,
+		Prefix:          "clb",
+		Subjects:        []string{"foo", "bar"},
+		Subscriptions:   []*nats.Subscription{},
+		Chart:           ch,
+		ChartPath:       cp,
+		ValuesPath:      pwd + "/../../hack/ci/values.yaml",
+	}
+
+	testCases := []testCase{
+		{
+			name:        "create message",
+			expectError: false,
+			msg: pubsubx.Message{
+				EventType:  create,
+				SubjectURN: "urn:infratographer:load-balancer:07442309-182E-4498-BC41-EBD679A9A793",
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		suite.T().Run(tc.name, func(t *testing.T) {
+			s := srv
+			err := s.processLoadBalancerCreate(tc.msg)
+
+			if tc.expectError {
+				assert.Error(suite.T(), err)
+			} else {
+				assert.Nil(suite.T(), err)
+			}
+
+		})
+	}
+
 }
