94145e41aa33749f7ddc5dad9419a6c4aa482778
Tyler Auerbeck <tylerauerbeck@users.noreply.github.com>
2023-12-04 11:54:21 -0500

    Remove cleanup cooldown for now
    Signed-off-by: Tyler Auerbeck <tylerauerbeck@users.noreply.github.com>

--- internal/srv/task.go
+++ internal/srv/task.go
@@ -2,11 +2,6 @@ package srv
 
 import (
 	"context"
-	"time"
-)
-
-const (
-	lbCleanupCooldown = 5
 )
 
 // runner is a struct that manages the flow of messages for a given loadbalancer
@@ -46,13 +41,6 @@ func (r *runner) run() {
 	for {
 		select {
 		case <-r.quit:
-			for {
-				if len(r.buffer) == 0 {
-					time.Sleep(lbCleanupCooldown * time.Second)
-					break
-				}
-			}
-
 			return
 		default:
 			if len(r.buffer) > 0 {
